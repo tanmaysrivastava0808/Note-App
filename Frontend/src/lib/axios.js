9de3cd64780b98ad9491dfca1c3b436aca36e29d
tanmaysrivastava0808 <tanmaysrivastava0308@gmail.com>
2025-10-08 18:32:11 +0530

    Update axios.js

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/v1/api/notes" : "/api";
+const BASE_URL = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
   baseURL: BASE_URL,
